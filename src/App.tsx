41cd8226b13117d0a871b4d7022b027808665224
Katjana Kosic <ge56yuw@tum.de>
2025-01-14 18:39:49 +0100

    Refactor App and main

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import './App.css'
+// App.tsx sets up the main application structure, including routing.
+// It applies background styling and defines routes for the application.
+
+import "./App.css"
+import { Route, Routes } from "react-router-dom"
 import { Box } from "@mui/material"
+import HomepageView from "./views/HomepageView"
 
 function App() {
-	return (
-		<Box>
-			
-		</Box>
-	)
+  return (
+    <Box
+      style={{
+        background: "#a6bae2",
+        width: "100%",
+        minHeight: "100vh",
+      }}
+    >
+      <Routes>
+        <Route path="/" element={<HomepageView />} />
+      </Routes>
+    </Box>
+  )
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
+// main.tsx is the entry point for the frontend application.
+// It sets up the React app and wraps it with BrowserRouter for routing.
+
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { BrowserRouter } from "react-router-dom"
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>
 )
