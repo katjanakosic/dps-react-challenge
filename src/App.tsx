a58621ed0f7cfd0e4b8996e0b912929ac2a7b260
Katjana Kosic <ge56yuw@tum.de>
2025-01-14 15:34:44 +0100

    Initial Project Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
-import dpsLogo from './assets/DPS.svg';
-import './App.css';
+import './App.css'
+import { Box } from "@mui/material"
 
 function App() {
 	return (
-		<>
-			<div>
-				<a href="https://www.digitalproductschool.io/" target="_blank">
-					<img src={dpsLogo} className="logo" alt="DPS logo" />
-				</a>
-			</div>
-			<div className="home-card">
-				<p>Your solution goes here 😊</p>
-			</div>
-		</>
-	);
+		<Box>
+			
+		</Box>
+	)
 }
 
-export default App;
+export default App
